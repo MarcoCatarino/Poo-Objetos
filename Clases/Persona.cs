8cf046f9ae33d1e6af0a604617bee422a069b6fe
MarcoCatarino <marco.catarino60qgmail.com>
2023-05-16 12:17:53 -0500

    Primer repositorio de GitHub

---
+++ Clases/Persona.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos.Clases
+{
+    // Las CLASES son plantillas y dentro de estos se encuentran OBJETOS y METODOS
+    // Se debe poner PUBLIC para poder usarlo de manera libre dentro del programa (otros objetos [clases])
+
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public double Salario { get; set; }
+    
+    
+        public void ImprmirDatos()
+        {
+            /* Este es la forma de pedir los DATOS AL USUARIO
+             
+            Console.Write("Nombre = ");
+            Nombre = Console.ReadLine();
+
+            Console.Write("Apellido = ");
+            Apellido = Console.ReadLine();
+
+            Console.Write("Edad = ");
+            Edad = int.Parse(Console.ReadLine());
+
+            Console.Write("Salario = ");
+            Salario = double.Parse(Console.ReadLine());
+
+            */
+
+            //Console.WriteLine("\n");
+
+            /* Esta es la forma de IMPRIMIR LOS DATOS (ya sea que ya esten declarados dentro del programa o el usuario sea quien los ingrese)
+             
+            // El "this" sirve pra poder usar de diversas manera una varieble u objeto previamente declarado de manera corracta. [Es una buena practica]
+
+            Console.WriteLine("Tu nombre es: " + this.Nombre);
+            Console.WriteLine("Tu apellido es: " + this.Apellido);
+            Console.WriteLine("Tu edad es: " +this.Edad);
+            Console.WriteLine("Tu salario es: " + this.Salario);
+
+            */
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Nombre = ");
+                Nombre = Console.ReadLine();
+
+                Console.Write("Apellido = ");
+                Apellido = Console.ReadLine();
+
+                Console.Write("Edad = ");
+                Edad = int.Parse(Console.ReadLine());
+
+                Console.Write("Salario = ");
+                Salario = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("\n");
+
+                // El "this" sirve pra poder usar de diversas manera una varieble u objeto previamente declarado de manera corracta. [Es una buena practica]
+                Console.WriteLine("Tu nombre es: " + this.Nombre);
+                Console.WriteLine("Tu apellido es: " + this.Apellido);
+                Console.WriteLine("Tu edad es: " + this.Edad);
+                Console.WriteLine("Tu salario es: " + this.Salario);
+
+                Console.WriteLine("\n");
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Objetos.Clases;
+
+namespace Objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INSTANCIAR = Crear algo "REAL" al mandarlo a llamar dentro del programa
+            Persona persona = new Persona();
+
+            persona.ImprmirDatos();
+
+            /* Este es el codigo en caso de que lo hicieramos 1 por 1 con valores ya definidos
+             
+            persona1.Nombre = "Marco";
+            persona1.Apellido = "Hernandez";
+            persona1.Edad = 19;
+            persona1.Salario = 121313.313D;
+
+            persona1.ImprmirDatos();
+
+            Console.WriteLine("\n");
+
+            Persona persona2 = new Persona();
+
+
+            persona2.Nombre = "johan";
+            persona2.Apellido = "La puta";
+            persona2.Edad = 18;
+            persona2.Salario = 00000;
+
+            persona2.ImprmirDatos();
+            
+            */
+        }
+    }
+}
